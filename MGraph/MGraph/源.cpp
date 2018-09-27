1fa97f70542089246e315b2b192ef400b4bf7240
longhuarst <841105197@qq.com>
2018-09-27 09:18:19 +0800

    增加邻接矩阵的图数据定义

---
+++ MGraph/MGraph/源.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+
+
+using namespace std;
+
+
+#define MaxVertexNUM 100
+typedef struct {
+
+	int Vertex[MaxVertexNUM];
+	int Edge[MaxVertexNUM][MaxVertexNUM];
+	int vexnum, arcnum;
+
+}MGraph;
+
+
+
+int main()
+{
+
+
+
+
+
+	system("pause");
+
+}
+
