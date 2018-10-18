3334b1cb9e48c286ac74b51db42cf306c6693863
longhuarst <841105197@qq.com>
2018-10-18 09:27:48 +0800

    插入排序验证

--- Sort_InsertSort/Sort_InsertSort/源.cpp
+++ Sort_InsertSort/Sort_InsertSort/源.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <random>
 using namespace std;
 
 
@@ -9,17 +9,59 @@ using namespace std;
 
 void InsertSort(int *data,int length)
 {
-
+	int tmp;
+	for (int i = 1; i < length; ++i) {
+		if (data[i - 1] > data[i]) {
+			tmp = data[i];
+			int j;
+			for (j = i; tmp < data[j - 1] && j>0; j--) {
+				data[j] = data[j - 1];
+			}
+			data[j] = tmp;
+		}
+	}
 }
 
+
+int myRandom(int min, int max)
+{
+	static default_random_engine e;
+
+	return (e() - e.min()) / (float)(e.max() - e.min()) * (max - min) + min;
+
+
+}
 
 
 int main(int argc, char *argv)
 {
 
+	
+
+	
+
+	int data[10];
+
+	cout << "生成序列： ";
+	for (int i = 0; i < 10; ++i) {
+		data[i] = myRandom(0,100);
+		cout << data[i] << " ";
+	}
+	cout << endl;
 
 
+	cout << "进行插入排序..." << endl;
 
+	InsertSort(data, sizeof(data) / sizeof(int));
+
+	cout << "生成序列： ";
+	for (int i = 0; i < 10; ++i) {
+		cout << data[i] << " ";
+	}
+	cout << endl;
+
+
+	system("pause");
 
 
 }
